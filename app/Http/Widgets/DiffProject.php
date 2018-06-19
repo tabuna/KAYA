5f6f86ef40151e3d8bd987ec7f4f3bce69c2b459
tabuna <bliz48rus@gmail.com>
2018-06-20 01:20:27 +0300

    up

--- app/Http/Widgets/DiffProject.php
+++ app/Http/Widgets/DiffProject.php
@@ -15,28 +15,32 @@ class DiffProject extends Widget {
      */
     public function handler($arguments = null){
 
-      $projects = Auth::user()->teams()->get();
-      $statics = [];
+      try {
+        $projects = Auth::user()->teams()->get();
+        $statics = [];
 
-      $days = [6,5,4,3,2,1,0];
+        $days = [6, 5, 4, 3, 2, 1, 0];
 
-      foreach ($days as $day){
-        foreach ($projects as $key => $project) {
-          $statics[$project->name][$day] = $project->log()
-            ->whereDate('created_at', Carbon::now()->subDay($day)->toDateString())
-            ->count();
+        foreach ($days as $day) {
+          foreach ($projects as $key => $project) {
+            $statics[$project->name][$day] = $project->log()
+              ->whereDate('created_at', Carbon::now()->subDay($day)->toDateString())
+              ->count();
+          }
         }
-      }
 
-      foreach ($days as $key => $day){
-        $days[$key] = Carbon::now()->subDay($day)->toDateString();
-      }
+        foreach ($days as $key => $day) {
+          $days[$key] = Carbon::now()->subDay($day)->toDateString();
+        }
 
 
-        return view('welcome',[
+        return view('welcome', [
           'days'    => $days,
           'statics' => $statics,
         ]);
+      }catch (\Exception $exception){
+        return;
+      }
     }
 
 }
